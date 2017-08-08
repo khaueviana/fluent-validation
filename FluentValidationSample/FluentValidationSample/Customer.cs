ac74a75707e32f46dcba2a2cc53e17f49794d495
Khauê Viana <khaue.viana@concrete.com.br>
2017-08-08 18:06:27 -0300

    Initial Commit

---
+++ FluentValidationSample/FluentValidationSample/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace FluentValidationSample
+{
+    public class Customer
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string DocumentId { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
---
+++ FluentValidationSample/FluentValidationSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FluentValidationSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
